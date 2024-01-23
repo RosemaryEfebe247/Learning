e9f5825c2d82a7756ac3414f236f0bfabf5fdf4f
Rosemaryefebe247 <efebeoghenekaro@gmail.com>
2024-01-24 00:32:07 +0100

    update

--- typegraphql_project/src/index.ts
+++ typegraphql_project/src/index.ts
@@ -1,1 +1,20 @@
-console.log("Hello World! Welcome")
+import dotenv from "dotenv";
+dotenv.config();
+import "reflect-metadata"
+import express from "express"
+
+async function bootstrap() {
+    // Build the schema
+
+    // Initialize express
+
+    // Create the apollo server
+
+    //await server.start
+
+    //apply middleware to the server
+
+// call app.listen on express server
+
+// connect to the database
+}
